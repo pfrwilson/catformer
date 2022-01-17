ebb239225f8019c5f02a9b1ab4ccb8f74f828804
Paul Wilson <pfrwilson@gmail.com>
2022-01-17 17:59:03 -0500

    moved data_dir from yaml to hard coded

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ from omegaconf import DictConfig
 import hydra
 from src.systems.vit_system import ViTSystem
 import pytorch_lightning as pl
+import os
+
+
+DATA_DIRECTORY = os.path.join(
+    os.getenv('~'), 
+    'data', 
+    'dogs-vs-cats', 
+    'train'
+)
+
 
 @hydra.main(config_path = '.', config_name='config')
 def main(config: DictConfig):
     
     system = ViTSystem(config.model.pretrained_transformer_name, 
-                       config.dataset.root_dir, 
+                       DATA_DIRECTORY, 
                        config.train.batch_size)
     
     trainer = pl.Trainer()
