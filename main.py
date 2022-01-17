8e59d9fb9f5c5ab29fe8abf53c20b14437ccf31e
Paul Wilson <pfrwilson@gmail.com>
2022-01-17 18:25:12 -0500

    fix parameter argument

--- main.py
+++ main.py
@@ -16,20 +16,16 @@ DEFAULT_MODEL_PATH = 'google/vit-large-patch16-384'
 
 def main(
         data_directory: str = DATA_DIRECTORY, 
-        num_gpus: Optional[int] = None, 
         max_epochs: Optional[int] = None, 
         model_path: str = DEFAULT_MODEL_PATH,
         batch_size: int = 128,
     ):
     
-    if num_gpus is None:
-        num_gpus = 1 if torch.cuda.is_available() else None
-    
     system = ViTSystem(model_path, 
                        data_directory, 
                        batch_size)
     
-    trainer = pl.Trainer(num_gpus=num_gpus, max_epochs=max_epochs)
+    trainer = pl.Trainer(accelerator='auto', max_epochs=max_epochs)
     trainer.fit(system)
 
 if __name__ == "__main__":
