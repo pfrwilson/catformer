02fbdbf27d435e2811f54a735ea13ac2c1a0e24b
Paul Wilson <pfrwilson@gmail.com>
2022-01-18 17:54:20 -0500

    changed config, moved dataloading logic

--- src/systems/vit_system.py
+++ src/systems/vit_system.py
@@ -2,19 +2,17 @@ import pytorch_lightning as pl
 import torch 
 from torch import nn 
 from torchmetrics import Accuracy
-from transformers import ViTForImageClassification, ViTConfig
+from transformers import ViTForImageClassification
 from omegaconf import DictConfig
 from ..data.cats_dogs import DogsVsCats
 from torch.utils.data import random_split, DataLoader
 from ..data.utils import TransformDataset
+from omegaconf import OmegaConf
+
 
 class ViTSystem(pl.LightningModule):
     
-    def __init__(self,
-                 pretrained_model_name_or_path: str, 
-                 data_root,
-                 batch_size,
-                 num_dataloader_workers=8):
+    def __init__(self, config: OmegaConf):
         """
         Instantiates a trainable lightning module for cat/dog classification.
         Parameters:
@@ -22,33 +20,50 @@ class ViTSystem(pl.LightningModule):
         """
         super().__init__()
         
-        vit = ViTForImageClassification.from_pretrained(
-            pretrained_model_name_or_path
+        self.config = config
+        self.save_hyperparameters(config)
+        
+        # pretrained vit model
+        self.pretrained_vit = ViTForImageClassification.from_pretrained(
+            self.config.pretrained_model_name_or_path
         )
-        self.data_root = data_root
-        self.vit = vit.vit
-        for parameter in list(self.vit.parameters()):
-            parameter.requires_grad = False
-        self.image_size = vit.config.image_size
-        self.hidden_size = vit.config.hidden_size 
-        self.classifier = nn.Linear(self.hidden_size, 2)
+        
+        if self.config.freeze_encoder_weights:
+            for parameter in list(self.pretrained_vit.vit.parameters()):   
+                parameter.requires_grad = False
+    
+        self.id2label = {
+            0: 'cat', 
+            1: 'dog'
+        }
+        self.label2id = {
+            'cat': 0, 
+            'dog': 1
+        }
+        self.classifier = nn.Linear(
+            self.pretrained_vit.config.hidden_size, 2
+        )
         self.loss_fn = torch.nn.CrossEntropyLoss()
-        self.batch_size = batch_size
-        self.num_dataloader_workers = num_dataloader_workers
         
     def forward(self, x, output_attentions=False):
     
-        transformer_output = self.vit(x)[0]
-        class_token = transformer_output[:, 0, :]
+        transformer_output = self.pretrained_vit.vit(
+            x, output_attentions=output_attentions
+        )
+        
+        last_hidden_layer = transformer_output[0]
+        attentions = transformer_output[1] if output_attentions else None
+        
+        class_token = last_hidden_layer[:, 0, :]
 
         logits = self.classifier(class_token)
         
-        return logits
+        return logits, attentions
         
     def training_step(self, batch, batch_idx):
         
         x, y = batch
-        logits = self(x)
+        logits = self(x)[0]
         y_hat = torch.argmax(logits, dim=-1)
         batch_size = y_hat.shape[0]
         
@@ -69,47 +84,10 @@ class ViTSystem(pl.LightningModule):
         loss = self.loss_fn(logits, y)
         accuracy = torch.sum(y_hat == y)/batch_size
         
-        self.log('val_loss', loss, prog_bar=True, on_step=True, on_epoch=True)
+        self.log('val_loss', loss, on_step=True, on_epoch=True)
         self.log('val_accuracy', accuracy, prog_bar=True, on_step=True, on_epoch=True)
         return loss
     
     def configure_optimizers(self):
-        return torch.optim.Adam(self.classifier.parameters())
+        return torch.optim.Adam(self.parameters())
     
-    def setup(self, stage): 
-        
-        self.full_ds = DogsVsCats(self.data_root)
-        
-        train_length = int(0.8 * len(self.full_ds))
-        test_length = len(self.full_ds) - train_length
-        
-        self.train_ds, self.val_ds = random_split(
-            self.full_ds,
-            [train_length, test_length], 
-            generator = torch.Generator().manual_seed(42)
-        )
-        
-        # Apply augmentations by wrapping in map dataset
-        self.train_ds = TransformDataset(
-            self.train_ds, 
-            DogsVsCats.get_default_transform(
-                target_size = self.image_size,
-                use_augmentations = True
-            )
-        )
-        self.val_ds = TransformDataset(
-            self.val_ds, 
-            DogsVsCats.get_default_transform(
-                target_size = self.image_size, 
-                use_augmentations=False
-            )
-        )
-        
-    def train_dataloader(self):
-        return DataLoader(self.train_ds, batch_size=self.batch_size, 
-                          num_workers=self.num_dataloader_workers)
-    
-    def val_dataloader(self):
-        return DataLoader(self.val_ds, batch_size=self.batch_size, 
-                          num_workers=self.num_dataloader_workers)
-        
