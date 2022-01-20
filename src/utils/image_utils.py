6c3edd16d4af9794335ba052a46bfa5038a5549a
Paul Wilson <pfrwilson@gmail.com>
2022-01-20 17:41:04 -0500

    added support for PIL images in resize_like

--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -28,11 +28,30 @@ def get_sample_mask(size=32):
         
 def resize_like(target_img, source_img):
     
+    if isinstance(source_img, np.ndarray):
+        source_img = Image.fromarray(source_img, 'P')
+    
     h = target_img.height
     w = target_img.width
     
-    return source_img.resize((h, w))
+    return source_img.resize((w, h))
     
 def add_opacity_mask(image: Image, mask: Image):
     image.putalpha(mask)
     return image
+
+def equalize(array):
+    """performs histogram equalization to correct exposure"""
+    return skimage.exposure.equalize_hist(array)
+
+def float_array_to_pil(array):
+    """Converts the given array with pixel values in the range (0, 1) and 1 channel to 
+    a PIL.Image instance
+
+    Args:
+        array ([type]): the array to convert
+    """
+    
+    array = np.uint8(array * 255)
+    return Image.fromarray(array, 'L')
+    
